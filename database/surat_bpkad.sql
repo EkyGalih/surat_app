6a280ca83dedadcebca15d1c8ca1dffdc2293701
EkyGalih <ekkygalih8@gmail.com>
2023-07-07 17:03:53 +0800

    add database

---
+++ database/surat_bpkad.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 07 Jul 2023 pada 17.03
+-- Versi server: 8.0.33-0ubuntu0.22.04.3
+-- Versi PHP: 8.1.2-1ubuntu2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `surat_bpkad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `distribusi`
+--
+
+CREATE TABLE `distribusi` (
+  `id` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surat_id` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bidang_id` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `file_surat`
+--
+
+CREATE TABLE `file_surat` (
+  `id` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surat_id` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `file_surat` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(2, '2023_07_07_021724_create_surat', 1),
+(3, '2023_07_07_025127_create_distribusi', 1),
+(4, '2023_07_07_025401_create_file_surat', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `surat`
+--
+
+CREATE TABLE `surat` (
+  `id` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dinas` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_masuk` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_surat` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_surat` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `disposisi` text COLLATE utf8mb4_unicode_ci,
+  `uraian` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_surat` enum('biasa','undangan','disposisi') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanda_terima` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `distribusi`
+--
+ALTER TABLE `distribusi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `distribusi_surat_id_index` (`surat_id`),
+  ADD KEY `distribusi_bidang_id_index` (`bidang_id`);
+
+--
+-- Indeks untuk tabel `file_surat`
+--
+ALTER TABLE `file_surat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `file_surat_surat_id_index` (`surat_id`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `surat`
+--
+ALTER TABLE `surat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `distribusi`
+--
+ALTER TABLE `distribusi`
+  ADD CONSTRAINT `distribusi_ibfk_1` FOREIGN KEY (`surat_id`) REFERENCES `surat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `file_surat`
+--
+ALTER TABLE `file_surat`
+  ADD CONSTRAINT `file_surat_ibfk_1` FOREIGN KEY (`surat_id`) REFERENCES `surat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
